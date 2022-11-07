b342ca1966fa87f67ac8e1794cd466bf6bb54127
adam.fierek@wykladowcy.pwsz.eu <adam.fierek@wykladowcy.pwsz.eu>
2022-11-07 13:27:10 +0100

    -Implemented MVVM pattern -Implemented basic functionality

---
+++ CalculatorWPF/ServiceLocator.cs
@@ -0,0 +1,30 @@
+﻿using CalculatorWPF.ViewModel;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorWPF
+{
+    public class ServiceLocator
+    {
+        private readonly ServiceProvider serviceProvider;
+
+        public ServiceLocator()
+        {
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            serviceProvider = serviceCollection.BuildServiceProvider();
+        }
+
+        private void ConfigureServices(ServiceCollection services)
+        {
+            services.AddSingleton<MainWindowViewModel>();
+        }
+
+        public MainWindowViewModel MainWindowVM => 
+            serviceProvider.GetService<MainWindowViewModel>();
+    }
+}
---
+++ CalculatorWPF/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CalculatorWPF.ViewModel
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private string numberString = "";
+        private List<string> components = new List<string>();
+        public string EquationValue => string.Join(null, components) + numberString;
+        public string ResultValue { get; set; } = "";
+        public Command Exit { get; }
+        public Command<string> TypeNumber { get; }
+        public Command TypeCancel { get; }
+        public Command<string> TypeOperation { get; }
+        public Command TypeEquals { get; }
+
+        public MainWindowViewModel()
+        {
+            Exit = new Command(_Exit);
+            TypeNumber = new Command<string>(_TypeNumber);
+            TypeCancel = new Command(_TypeCancel);
+            TypeOperation = new Command<string>(_TypeOperation);
+            TypeEquals = new Command(_TypeEquals);
+        }
+
+        private void _TypeEquals()
+        {
+            if (numberString != "")
+            {
+                components.Add(numberString);
+                numberString = "";
+            }
+            ResultValue = GetResult();
+            RaisePropertyChanged(nameof(ResultValue));
+        }
+
+        private string GetResult()
+        {
+            //TODO: implement ONP equation solver
+            return "xDD";
+        }
+
+        private void _TypeOperation(string obj)
+        {
+            if (numberString != "")
+            {
+                components.Add(numberString);
+                numberString = "";
+            }
+
+            components.Add(obj);
+            RaisePropertyChanged(nameof(EquationValue));
+        }
+
+        private void _TypeCancel()
+        {
+            if (ResultValue != "")
+            {
+                components.Clear();
+                ResultValue = "";
+            }
+            else if (numberString != "")
+            {
+                numberString = numberString.Substring(0, numberString.Length - 1);
+            }
+            else if (components.Count > 0)
+            {
+                components.RemoveAt(components.Count - 1);
+            }
+
+            RaisePropertyChanged(nameof(EquationValue));
+            RaisePropertyChanged(nameof(ResultValue));
+        }
+
+        private void _TypeNumber(string obj)
+        {
+            numberString += obj;
+            RaisePropertyChanged(nameof(EquationValue));
+        }
+
+        private void _Exit()
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
